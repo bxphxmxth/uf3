bff4178dacb40157b9b6867050b1827465637574
bxphxmxth <81629915+bxphxmxth@users.noreply.github.com>
2021-09-12 17:43:28 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ let goYou = function changeYou() {
 
    window.open("https://www.youtube.com/channel/UCeK4tuluBGPXEBKOHbE9m5g", "_blank") 
 }
-// window.open(URL, '_blank');
 youtube.onclick = goYou
 
 // Instagram fonction click 
@@ -25,7 +24,6 @@ let goIn = function changeYou() {
 
    window.open("https://www.instagram.com/unhealthy_fantasies/", "_blank") 
 }
-// window.open(URL, '_blank');
 instagram.onclick = goIn
 
 // Tiktok fonction click 
@@ -41,8 +39,41 @@ let goTik = function changeYou() {
     tiktok.style.color = "purple";
 
    window.open("https://www.tiktok.com/@unhealthy_fantasies?lang=fr", "_blank") 
-}
+};
 // window.open(URL, '_blank');
 tiktok.onclick = goTik
 
 
+let bandcamp = document.getElementsByClassName("fa-bandcamp")[0];
+console.log(bandcamp);
+
+
+let goBand = function changeBand() {
+   
+
+    bandcamp.style.color = "purple";
+
+   window.open("https://unhealthyfantasi3s.bandcamp.com/", "_blank") 
+}
+bandcamp.onclick = goBand
+
+
+// Inverted logo
+
+let cat = document.getElementsByClassName("leLogo")[0];
+console.log(cat)
+
+let invert = function changeColor(){
+
+    if(cat.style.filter = 0){
+
+        cat.style.filter ="invert(100%)";
+
+    }else{
+        cat.classList.toggle("logoInvert");
+    }
+
+    }
+
+
+cat.onclick = invert;
